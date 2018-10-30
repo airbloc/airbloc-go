e182f65607dca67a57b229838021eaa8f84fdaa1
hongbyeongsu <frostornge@gmail.com>
2018-10-30 19:09:12 +0900

    exchange: Add api base

---
+++ exchange/exchange_api.go
@@ -0,0 +1,41 @@
+package exchange
+
+import (
+	"net"
+
+	"github.com/airbloc/airbloc-go/common"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+type Service struct {
+	conn   net.Conn
+	server *grpc.Server
+}
+
+func (s *Service) Close() {
+	s.server.Stop()
+	s.conn.Close()
+}
+
+// TODO
+func (s *Service) Order(ctx context.Context, req *OrderRequest) (*OrderId, error) {
+	return nil, nil
+}
+
+func (s *Service) Settle(ctx context.Context, id *OrderId) (*SettleResult, error) {
+	return nil, nil
+}
+
+func (s *Service) Reject(ctx context.Context, id *OrderId) (*common.Result, error) {
+	return nil, nil
+}
+
+func NewService(conn net.Conn) (*Service, error) {
+	service := &Service{
+		conn:   conn,
+		server: grpc.NewServer(),
+	}
+	RegisterExchangeServiceServer(service.server, service)
+	return service, nil
+}
