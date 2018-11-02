81179d7cd2b05c2c80be641f235ca018e556a2e4
therne <retail3210@gmail.com>
2018-11-02 15:06:03 +0900

    data: Basic IDs and Data Types

---
+++ data/types.go
@@ -0,0 +1,45 @@
+package data
+
+import (
+	"fmt"
+	"github.com/airbloc/airbloc-go/common"
+	"github.com/pkg/errors"
+	"strconv"
+)
+
+type Data struct {
+	Payload   string `json:"payload"`
+	OwnerAnid string `json:"ownerAnid"`
+}
+
+type EncryptedData struct {
+	Data
+	Payload []byte `json:"payload"`
+	Capsule []byte `json:"capsule"`
+}
+
+type ID struct {
+	BundleID common.ID
+	Index    int
+}
+
+func NewDataID(dataId string) (*ID, error) {
+	bundleId, err := common.IDFromString(dataId[:common.IDLength])
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse data ID from the given data ID.")
+	}
+
+	index, err := strconv.Atoi(dataId[common.IDLength+1:])
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse data index from the given data ID.")
+	}
+
+	return &ID{
+		BundleID: bundleId,
+		Index:    index,
+	}, nil
+}
+
+func (id *ID) String() string {
+	return fmt.Sprintf("%s/%d", id.BundleID.String(), id.Index)
+}
