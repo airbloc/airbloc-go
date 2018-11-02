600d832fa09ac6f720a1c60eb6f0c4d68940ecd2
hongbyeongsu <frostornge@gmail.com>
2018-11-02 15:49:58 +0900

    bind: Add contract-golang bind

--- account/account.go
+++ account/account.go
@@ -1,1 +1,35 @@
 package account
+
+import (
+	"crypto/ecdsa"
+
+	ablCommon "github.com/airbloc/airbloc-go/common"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+)
+
+type Status int8
+
+const (
+	StatusNone = Status(iota)
+	StatusTemporary
+	StatusCreated
+)
+
+type Account struct {
+	ID          ablCommon.ID
+	Status      Status
+	Owner       ethCommon.Address
+	Proxy       ethCommon.Address
+	PasswordSig []byte
+
+	Opts *bind.TransactOpts
+	key  *ecdsa.PrivateKey
+}
+
+func NewAccount(key *ecdsa.PrivateKey) *Account {
+	return &Account{
+		Opts: bind.NewKeyedTransactor(key),
+		key:  key,
+	}
+}
