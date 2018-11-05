ddc5a045ed6e4f3d24a05d6a89d6ed48adae4ed5
therne <retail3210@gmail.com>
2018-11-06 00:53:22 +0900

    account: Implement Account API

--- account/account_api.go
+++ account/account_api.go
@@ -1,32 +1,32 @@
 package account
 
 import (
-	"net"
-
 	"context"
-
-	"google.golang.org/grpc"
+	"github.com/airbloc/airbloc-go/api"
+	ablCommon "github.com/airbloc/airbloc-go/common"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
 )
 
 type API struct {
-	conn    net.Conn
-	server  *grpc.Server
-	service *Service
+	manager *Manager
+}
+
+func NewAPI(backend *api.AirblocBackend) (api.API, error) {
+	manager, err := NewManager(backend.Ethclient, ethCommon.Address{})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create AccountManager")
+	}
+	return &API{manager}, nil
+}
+
+func (api *API) AttachToAPI(service *api.APIService) {
+	RegisterAccountServer(service.GrpcServer, api)
 }
 
 func (api *API) Create(ctx context.Context, req *AccountCreateRequest) (*AccountCreateResponse, error) {
-	id, err := api.service.Create(ctx)
+	id, err := api.manager.Create(ctx)
 	return &AccountCreateResponse{
-		AccountId: id[:],
+		AccountId: &ablCommon.Hash{Hash: id[:]},
 	}, err
-}
-
-func NewAPI(conn net.Conn, service *Service) (*API, error) {
-	api := &API{
-		conn:    conn,
-		server:  grpc.NewServer(),
-		service: service,
-	}
-	RegisterAccountServer(api.server, api)
-	return api, nil
 }
