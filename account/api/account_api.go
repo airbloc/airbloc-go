ae1904b2919d96eeb09258e621fd6378544c31ea
therne <retail3210@gmail.com>
2018-11-16 18:26:26 +0900

    api: Login API for Account

--- account/api/account_api.go
+++ account/api/account_api.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"github.com/airbloc/airbloc-go/account"
 	"github.com/airbloc/airbloc-go/api"
-	"github.com/ethereum/go-ethereum/common"
+	ablCommon "github.com/airbloc/airbloc-go/common"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
 )
 
 type API struct {
@@ -21,7 +23,50 @@ func (api *API) AttachToAPI(service *api.APIService) {
 }
 
 func (api *API) Create(ctx context.Context, req *AccountCreateRequest) (*AccountCreateResponse, error) {
-	address := common.BytesToAddress(req.GetAddress())
+	address := ethCommon.BytesToAddress(req.GetAddress())
 	id, err := api.manager.CreateUsingProxy(address, req.GetPasswordSignature())
 	return &AccountCreateResponse{AccountId: id.String()}, err
+}
+
+func (api *API) Get(ctx context.Context, req *AccountGetRequest) (*AccountGetResponse, error) {
+	accountId, err := ablCommon.IDFromString(req.GetAccountId())
+	if err != nil {
+		return nil, errors.Wrapf(err, "unable to parse account ID: %s", req.GetAccountId())
+	}
+
+	acc, err := api.manager.Get(accountId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AccountGetResponse{
+		AccountId:     acc.ID.String(),
+		OwnerAddress:  acc.Owner.Bytes(),
+		Status:        AccountGetResponse_Status(acc.Status),
+		ProxyAddress:  acc.Proxy.Bytes(),
+		PasswordProof: acc.PasswordProof.Bytes(),
+	}, nil
+}
+
+func (api *API) GetByIdentity(ctx context.Context, req *AccountGetByIdentityRequest) (*AccountGetResponse, error) {
+	acc, err := api.manager.GetByIdentity(req.GetIdentity())
+	if err != nil {
+		return nil, err
+	}
+
+	return &AccountGetResponse{
+		AccountId:     acc.ID.String(),
+		OwnerAddress:  acc.Owner.Bytes(),
+		Status:        AccountGetResponse_Status(acc.Status),
+		ProxyAddress:  acc.Proxy.Bytes(),
+		PasswordProof: acc.PasswordProof.Bytes(),
+	}, nil
+}
+
+func (api *API) TestPassword(ctx context.Context, req *TestPasswordRequest) (*TestPasswordResponse, error) {
+	exists, err := api.manager.TestPassword(
+		ethCommon.BytesToHash(req.GetMessageHash()),
+		req.GetSignature())
+
+	return &TestPasswordResponse{Exists: exists}, err
 }
