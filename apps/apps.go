0e857a2d687fd7cebe0b473bc06bfa137b704898
hongbyeongsu <frostornge@gmail.com>
2018-11-21 19:11:42 +0900

    apps: Add api and contract manager

--- apps/apps.go
+++ apps/apps.go
@@ -1,1 +1,106 @@
 package apps
+
+import (
+	"context"
+
+	"github.com/airbloc/airbloc-go/adapter"
+	"github.com/airbloc/airbloc-go/blockchain"
+	ablCommon "github.com/airbloc/airbloc-go/common"
+	ethCommon "github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
+)
+
+var (
+	ErrPrefix          = "apps : "
+	ErrCannotCreateTx  = ErrPrefix + "failed to create tx"
+	ErrCannotCall      = ErrPrefix + "call failed"
+	ErrContext         = ErrPrefix + "context error"
+	ErrTxReverted      = ErrPrefix + "tx reverted"
+	ErrEventParseError = ErrPrefix + "failed to parse event from receipt"
+)
+
+type Apps struct {
+	client   blockchain.TxClient
+	contract *adapter.AppRegistry
+}
+
+func New(client blockchain.TxClient) (*Apps, error) {
+	raw, err := client.GetContract(&adapter.AppRegistry{})
+	if err != nil {
+		return nil, err
+	}
+
+	contract, ok := raw.(*adapter.AppRegistry)
+	if !ok {
+		return nil, blockchain.ErrContractNotFound
+	}
+
+	return &Apps{
+		client:   client,
+		contract: contract,
+	}, nil
+}
+
+func (apps *Apps) NewOwner(ctx context.Context, appId ablCommon.ID, newOwner ethCommon.Address) (bool, error) {
+	tx, err := apps.contract.NewOwner(apps.client.Account(), appId, newOwner)
+	if err != nil {
+		return false, errors.Wrap(err, ErrCannotCreateTx)
+	}
+
+	_, err = apps.client.WaitMined(ctx, tx)
+	if ctx.Err() != nil {
+		return false, errors.Wrap(err, ErrContext)
+	}
+
+	if err != nil {
+		return false, errors.Wrap(err, ErrTxReverted)
+	}
+	return true, nil
+}
+
+func (apps *Apps) CheckOwner(ctx context.Context, appId ablCommon.ID, owner ethCommon.Address) (bool, error) {
+	result, err := apps.contract.CheckOwner(nil, appId, owner)
+	if err != nil {
+		return false, errors.Wrap(err, ErrCannotCall)
+	}
+	return result, nil
+}
+
+func (apps *Apps) Register(ctx context.Context, name string) (ablCommon.ID, error) {
+	tx, err := apps.contract.Register(apps.client.Account(), name)
+	if err != nil {
+		return ablCommon.ID{}, errors.Wrap(err, ErrCannotCreateTx)
+	}
+
+	receipt, err := apps.client.WaitMined(ctx, tx)
+	if ctx.Err() != nil {
+		return ablCommon.ID{}, errors.Wrap(err, ErrContext)
+	}
+
+	if err != nil {
+		return ablCommon.ID{}, errors.Wrap(err, ErrTxReverted)
+	}
+
+	event, err := apps.contract.ParseRegisteredFromReceipt(receipt)
+	if err != nil {
+		return ablCommon.ID{}, errors.Wrap(err, ErrEventParseError)
+	}
+	return event.AppId, nil
+}
+
+func (apps *Apps) Unregister(ctx context.Context, appId ablCommon.ID) (bool, error) {
+	tx, err := apps.contract.Unregister(apps.client.Account(), appId)
+	if err != nil {
+		return false, errors.Wrap(err, ErrCannotCreateTx)
+	}
+
+	_, err = apps.client.WaitMined(ctx, tx)
+	if ctx.Err() != nil {
+		return false, errors.Wrap(err, ErrContext)
+	}
+
+	if err != nil {
+		return false, errors.Wrap(err, ErrTxReverted)
+	}
+	return true, nil
+}
