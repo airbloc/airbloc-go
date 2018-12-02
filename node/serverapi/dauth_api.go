52100575181e23206c8f3bcf33ecc8643f8d8e42
therne <retail3210@gmail.com>
2018-12-03 03:17:30 +0900

    dauth: Finalize Provider API

--- node/serverapi/dauth_api.go
+++ node/serverapi/dauth_api.go
@@ -2,69 +2,57 @@ package serverapi
 
 import (
 	"context"
-	"github.com/airbloc/airbloc-go/account"
-	"github.com/azer/logger"
-	"log"
-
 	"github.com/airbloc/airbloc-go/common"
 	"github.com/airbloc/airbloc-go/dauth"
 	"github.com/airbloc/airbloc-go/node"
 	commonpb "github.com/airbloc/airbloc-go/proto/rpc/v1"
 	pb "github.com/airbloc/airbloc-go/proto/rpc/v1/server"
-	"github.com/pkg/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type DAuthAPI struct {
-	accountManager *account.Manager
-	dauthManager   *dauth.Manager
+	dauthClient *dauth.ProviderClient
 }
 
 func NewDAuthAPI(backend node.Backend) (node.API, error) {
-	return &DAuthAPI{
-		accountManager: account.NewManager(backend.Client()),
-		dauthManager:   dauth.NewManager(backend.Client()),
-	}, nil
+	dauthClient := dauth.NewProviderClient(backend.Kms(), backend.Client(), backend.P2P())
+	return &DAuthAPI{dauthClient}, nil
 }
 
 func (api *DAuthAPI) SignIn(ctx context.Context, req *pb.SignInRequest) (*pb.SignInResponse, error) {
-	// check that user has been already signed up
-	acc, err := api.accountManager.GetByIdentity(req.GetIdentity())
+	accountId, err := api.dauthClient.SignIn(ctx, req.GetIdentity(), req.GetUserDelegate())
 	if err != nil {
-		if err == account.ErrNoAccount {
-			api.accountManager.CreateTemporary()
-
-		}
-		return nil, errors.Wrap(err, "unable to call Accounts.GetByIdentity")
+		return nil, err
 	}
-	api.log.Info("SignIn(\"%s\"): Already signed up.", req.GetIdentity(), logger.Attrs{
-		"id":     acc.ID.String(),
-		"status": acc.Status,
-		"proxy":  acc.Proxy.Hex(),
-	})
 	return &pb.SignInResponse{
-		AccountId: acc.ID.String(),
+		AccountId: accountId.String(),
 	}, nil
 }
 
 func (api *DAuthAPI) Allow(ctx context.Context, req *pb.DAuthRequest) (*commonpb.Result, error) {
 	collectionId, err := common.IDFromString(req.GetCollectionId())
 	if err != nil {
-		return nil, errors.Wrapf(err, "invalid collection ID: %s", req.GetCollectionId())
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid collection ID: %s", req.GetCollectionId())
 	}
-
-	// err = api.dauthManager.Allow(collectionId, req.GetPasswordSignature())
-	log.Println(collectionId.String())
+	accountId, err := common.IDFromString(req.GetAccountId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid account ID: %s", req.GetAccountId())
+	}
+	err = api.dauthClient.Allow(ctx, collectionId, accountId)
 	return &commonpb.Result{}, err
 }
 
 func (api *DAuthAPI) Deny(ctx context.Context, req *pb.DAuthRequest) (*commonpb.Result, error) {
 	collectionId, err := common.IDFromString(req.GetCollectionId())
 	if err != nil {
-		return nil, errors.Wrapf(err, "invalid collection ID: %s", req.GetCollectionId())
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid collection ID: %s", req.GetCollectionId())
 	}
-
-	log.Println(collectionId.String())
-	// err = api.dauthManager.Deny(collectionId, req.GetPasswordSignature())
+	accountId, err := common.IDFromString(req.GetAccountId())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid account ID: %s", req.GetAccountId())
+	}
+	err = api.dauthClient.Deny(ctx, collectionId, accountId)
 	return &commonpb.Result{}, err
 }
 
--- proto/rpc/v1/server/dauth.pb.go
+++ proto/rpc/v1/server/dauth.pb.go
@@ -23,9 +23,95 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+type SignInRequest struct {
+	Identity             string   `protobuf:"bytes,1,opt,name=identity,proto3" json:"identity,omitempty"`
+	UserDelegate         []byte   `protobuf:"bytes,2,opt,name=userDelegate,proto3" json:"userDelegate,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *SignInRequest) Reset()         { *m = SignInRequest{} }
+func (m *SignInRequest) String() string { return proto.CompactTextString(m) }
+func (*SignInRequest) ProtoMessage()    {}
+func (*SignInRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_66cc8b72c8d7fd76, []int{0}
+}
+
+func (m *SignInRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignInRequest.Unmarshal(m, b)
+}
+func (m *SignInRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignInRequest.Marshal(b, m, deterministic)
+}
+func (m *SignInRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignInRequest.Merge(m, src)
+}
+func (m *SignInRequest) XXX_Size() int {
+	return xxx_messageInfo_SignInRequest.Size(m)
+}
+func (m *SignInRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignInRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignInRequest proto.InternalMessageInfo
+
+func (m *SignInRequest) GetIdentity() string {
+	if m != nil {
+		return m.Identity
+	}
+	return ""
+}
+
+func (m *SignInRequest) GetUserDelegate() []byte {
+	if m != nil {
+		return m.UserDelegate
+	}
+	return nil
+}
+
+type SignInResponse struct {
+	AccountId            string   `protobuf:"bytes,1,opt,name=accountId,proto3" json:"accountId,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *SignInResponse) Reset()         { *m = SignInResponse{} }
+func (m *SignInResponse) String() string { return proto.CompactTextString(m) }
+func (*SignInResponse) ProtoMessage()    {}
+func (*SignInResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_66cc8b72c8d7fd76, []int{1}
+}
+
+func (m *SignInResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_SignInResponse.Unmarshal(m, b)
+}
+func (m *SignInResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_SignInResponse.Marshal(b, m, deterministic)
+}
+func (m *SignInResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_SignInResponse.Merge(m, src)
+}
+func (m *SignInResponse) XXX_Size() int {
+	return xxx_messageInfo_SignInResponse.Size(m)
+}
+func (m *SignInResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_SignInResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_SignInResponse proto.InternalMessageInfo
+
+func (m *SignInResponse) GetAccountId() string {
+	if m != nil {
+		return m.AccountId
+	}
+	return ""
+}
+
 type DAuthRequest struct {
 	CollectionId         string   `protobuf:"bytes,1,opt,name=collectionId,proto3" json:"collectionId,omitempty"`
-	IdentityHash         []byte   `protobuf:"bytes,2,opt,name=identityHash,proto3" json:"identityHash,omitempty"`
+	AccountId            string   `protobuf:"bytes,2,opt,name=accountId,proto3" json:"accountId,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -35,7 +121,7 @@ func (m *DAuthRequest) Reset()         { *m = DAuthRequest{} }
 func (m *DAuthRequest) String() string { return proto.CompactTextString(m) }
 func (*DAuthRequest) ProtoMessage()    {}
 func (*DAuthRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_66cc8b72c8d7fd76, []int{0}
+	return fileDescriptor_66cc8b72c8d7fd76, []int{2}
 }
 
 func (m *DAuthRequest) XXX_Unmarshal(b []byte) error {
@@ -63,35 +149,42 @@ func (m *DAuthRequest) GetCollectionId() string {
 	return ""
 }
 
-func (m *DAuthRequest) GetIdentityHash() []byte {
+func (m *DAuthRequest) GetAccountId() string {
 	if m != nil {
-		return m.IdentityHash
+		return m.AccountId
 	}
-	return nil
+	return ""
 }
 
 func init() {
+	proto.RegisterType((*SignInRequest)(nil), "airbloc.rpc.v1.SignInRequest")
+	proto.RegisterType((*SignInResponse)(nil), "airbloc.rpc.v1.SignInResponse")
 	proto.RegisterType((*DAuthRequest)(nil), "airbloc.rpc.v1.DAuthRequest")
 }
 
 func init() { proto.RegisterFile("proto/rpc/v1/server/dauth.proto", fileDescriptor_66cc8b72c8d7fd76) }
 
 var fileDescriptor_66cc8b72c8d7fd76 = []byte{
-	// 220 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x2f, 0x28, 0xca, 0x2f,
-	0xc9, 0xd7, 0x2f, 0x2a, 0x48, 0xd6, 0x2f, 0x33, 0xd4, 0x2f, 0x4e, 0x2d, 0x2a, 0x4b, 0x2d, 0xd2,
-	0x4f, 0x49, 0x2c, 0x2d, 0xc9, 0xd0, 0x03, 0xcb, 0x08, 0xf1, 0x25, 0x66, 0x16, 0x25, 0xe5, 0xe4,
-	0x27, 0xeb, 0x15, 0x15, 0x24, 0xeb, 0x95, 0x19, 0x4a, 0x49, 0xa0, 0x68, 0x28, 0xa9, 0x2c, 0x48,
-	0x2d, 0x86, 0xa8, 0x54, 0x0a, 0xe3, 0xe2, 0x71, 0x71, 0x2c, 0x2d, 0xc9, 0x08, 0x4a, 0x2d, 0x2c,
-	0x4d, 0x2d, 0x2e, 0x11, 0x52, 0xe2, 0xe2, 0x49, 0xce, 0xcf, 0xc9, 0x49, 0x4d, 0x2e, 0xc9, 0xcc,
-	0xcf, 0xf3, 0x4c, 0x91, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x42, 0x11, 0x03, 0xa9, 0xc9, 0x4c,
-	0x49, 0xcd, 0x2b, 0xc9, 0x2c, 0xa9, 0xf4, 0x48, 0x2c, 0xce, 0x90, 0x60, 0x52, 0x60, 0xd4, 0xe0,
-	0x09, 0x42, 0x11, 0x33, 0x6a, 0x61, 0xe4, 0x62, 0x05, 0x1b, 0x2c, 0x64, 0xcb, 0xc5, 0xea, 0x98,
-	0x93, 0x93, 0x5f, 0x2e, 0x24, 0xa3, 0x87, 0xea, 0x2a, 0x3d, 0x64, 0x8b, 0xa5, 0xc4, 0xd0, 0x65,
-	0x83, 0x52, 0x8b, 0x4b, 0x73, 0x4a, 0x84, 0x6c, 0xb8, 0x58, 0x5c, 0x52, 0xf3, 0x2a, 0xc9, 0xd3,
-	0xed, 0x64, 0x1c, 0x65, 0x98, 0x9e, 0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x0f,
-	0x55, 0x03, 0xa3, 0x75, 0xd3, 0xf3, 0xf5, 0xb1, 0x84, 0x64, 0x12, 0x1b, 0x58, 0xd0, 0x18, 0x10,
-	0x00, 0x00, 0xff, 0xff, 0xf3, 0x50, 0xee, 0xea, 0x67, 0x01, 0x00, 0x00,
+	// 290 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x9c, 0x92, 0x4d, 0x4f, 0x32, 0x31,
+	0x10, 0xc7, 0xb3, 0xe4, 0x81, 0x3c, 0x4c, 0x56, 0x0e, 0x3d, 0x18, 0xb2, 0x41, 0x25, 0x7b, 0xe2,
+	0x62, 0x1b, 0xe4, 0xaa, 0x07, 0x0c, 0x89, 0xe1, 0xa4, 0x59, 0x6f, 0xde, 0x96, 0x32, 0x59, 0x9a,
+	0xd4, 0x76, 0x6d, 0xa7, 0x18, 0x3e, 0xa4, 0xdf, 0xc9, 0x04, 0x58, 0xb4, 0x1b, 0xbd, 0x78, 0xda,
+	0xec, 0xff, 0xe5, 0xd7, 0x99, 0xa6, 0x70, 0x55, 0x3b, 0x4b, 0x56, 0xb8, 0x5a, 0x8a, 0xed, 0x54,
+	0x78, 0x74, 0x5b, 0x74, 0x62, 0x5d, 0x06, 0xda, 0xf0, 0xbd, 0xc3, 0x06, 0xa5, 0x72, 0x2b, 0x6d,
+	0x25, 0x77, 0xb5, 0xe4, 0xdb, 0x69, 0x36, 0x8c, 0x0a, 0xb4, 0xab, 0xd1, 0x1f, 0x92, 0xf9, 0x23,
+	0x9c, 0x3d, 0xab, 0xca, 0x2c, 0x4d, 0x81, 0x6f, 0x01, 0x3d, 0xb1, 0x0c, 0xfe, 0xab, 0x35, 0x1a,
+	0x52, 0xb4, 0x1b, 0x26, 0xe3, 0x64, 0xd2, 0x2f, 0x4e, 0xff, 0x2c, 0x87, 0x34, 0x78, 0x74, 0x0b,
+	0xd4, 0x58, 0x95, 0x84, 0xc3, 0xce, 0x38, 0x99, 0xa4, 0x45, 0xa4, 0xe5, 0x1c, 0x06, 0x0d, 0xd0,
+	0xd7, 0xd6, 0x78, 0x64, 0x23, 0xe8, 0x97, 0x52, 0xda, 0x60, 0x68, 0xb9, 0x3e, 0x22, 0xbf, 0x84,
+	0xfc, 0x09, 0xd2, 0xc5, 0x3c, 0xd0, 0xa6, 0x39, 0x3f, 0x87, 0x54, 0x5a, 0xad, 0x51, 0x92, 0xb2,
+	0xe6, 0x54, 0x88, 0xb4, 0x98, 0xd8, 0x69, 0x11, 0x6f, 0x3e, 0x12, 0xe8, 0xee, 0x91, 0xec, 0x01,
+	0x7a, 0x87, 0x59, 0xd8, 0x05, 0x8f, 0x6f, 0x84, 0x47, 0x4b, 0x67, 0x97, 0xbf, 0xd9, 0xc7, 0x15,
+	0xee, 0xa0, 0x3b, 0xd7, 0xda, 0xbe, 0xb3, 0x51, 0x3b, 0xf8, 0x7d, 0xf6, 0xec, 0xbc, 0xed, 0x16,
+	0xe8, 0x83, 0x26, 0x76, 0x0b, 0xff, 0x16, 0x68, 0x76, 0x7f, 0x6b, 0xdf, 0xcf, 0x5e, 0xa6, 0x95,
+	0xa2, 0x4d, 0x58, 0x71, 0x69, 0x5f, 0xc5, 0x31, 0xd3, 0x7c, 0xaf, 0x2b, 0x2b, 0x7e, 0x78, 0x0d,
+	0xab, 0xde, 0x5e, 0x9c, 0x7d, 0x06, 0x00, 0x00, 0xff, 0xff, 0xda, 0x01, 0x54, 0xc2, 0x2b, 0x02,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -106,6 +199,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type DAuthClient interface {
+	SignIn(ctx context.Context, in *SignInRequest, opts ...grpc.CallOption) (*SignInResponse, error)
 	Allow(ctx context.Context, in *DAuthRequest, opts ...grpc.CallOption) (*v1.Result, error)
 	Deny(ctx context.Context, in *DAuthRequest, opts ...grpc.CallOption) (*v1.Result, error)
 }
@@ -116,6 +210,15 @@ type dAuthClient struct {
 
 func NewDAuthClient(cc *grpc.ClientConn) DAuthClient {
 	return &dAuthClient{cc}
+}
+
+func (c *dAuthClient) SignIn(ctx context.Context, in *SignInRequest, opts ...grpc.CallOption) (*SignInResponse, error) {
+	out := new(SignInResponse)
+	err := c.cc.Invoke(ctx, "/airbloc.rpc.v1.DAuth/SignIn", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
 func (c *dAuthClient) Allow(ctx context.Context, in *DAuthRequest, opts ...grpc.CallOption) (*v1.Result, error) {
@@ -138,12 +241,31 @@ func (c *dAuthClient) Deny(ctx context.Context, in *DAuthRequest, opts ...grpc.C
 
 // DAuthServer is the server API for DAuth service.
 type DAuthServer interface {
+	SignIn(context.Context, *SignInRequest) (*SignInResponse, error)
 	Allow(context.Context, *DAuthRequest) (*v1.Result, error)
 	Deny(context.Context, *DAuthRequest) (*v1.Result, error)
 }
 
 func RegisterDAuthServer(s *grpc.Server, srv DAuthServer) {
 	s.RegisterService(&_DAuth_serviceDesc, srv)
+}
+
+func _DAuth_SignIn_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(SignInRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(DAuthServer).SignIn(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/airbloc.rpc.v1.DAuth/SignIn",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(DAuthServer).SignIn(ctx, req.(*SignInRequest))
+	}
+	return interceptor(ctx, in, info, handler)
 }
 
 func _DAuth_Allow_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -186,6 +308,10 @@ var _DAuth_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "airbloc.rpc.v1.DAuth",
 	HandlerType: (*DAuthServer)(nil),
 	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SignIn",
+			Handler:    _DAuth_SignIn_Handler,
+		},
 		{
 			MethodName: "Allow",
 			Handler:    _DAuth_Allow_Handler,
