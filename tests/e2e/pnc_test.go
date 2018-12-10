48edb40812324ab7ee0d937762827685aac862a7
hongbyeongsu <frostornge@gmail.com>
2018-12-10 11:43:16 +0900

    Revert "common: Fix minor errors"
    This reverts commit b4be1408

--- tests/e2e/pnc_test.go
+++ tests/e2e/pnc_test.go
@@ -15,25 +15,6 @@ import (
 	pb "github.com/airbloc/airbloc-go/proto/rpc/v1/server"
 )
 
-const numberOfUsers = 10
-const testSchema = `{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "http://airbloc.io/testdata.schema.json",
-  "title": "Test Data",
-  "type": "object",
-  "properties": {
-	"name": {
-      "description": "Who are you lol",
-      "type": "string"
-	},
-    "age": {
-      "description": "Age is Age",
-      "type": "integer"
-    }
-  },
-  "required": [ "name" ]
-}`
-
 func testCreateApp(t *testing.T, ctx context.Context, conn *grpc.ClientConn) string {
 	apps := pb.NewAppsClient(conn)
 	result, err := apps.Register(ctx, &pb.RegisterRequest{
@@ -108,51 +89,5 @@ func TestPurchaseData(t *testing.T) {
 }
 
 func TestPnc(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	conn, err := grpc.Dial("localhost:9124", grpc.WithInsecure())
-	if err != nil {
-		panic(err.Error())
-	}
-	defer func() { _ = conn.Close() }()
-
-	appId := testCreateApp(t, ctx, conn)
-	log.Println("Created App ID:", appId)
-
-	schemaId := testCreateSchema(t, ctx, conn)
-	log.Printf("Created Schema ID: %s\n", schemaId)
-
-	collectionId := testCreateCollection(t, ctx, appId, schemaId, conn)
-	log.Printf("Created Collection: %s\n", collectionId)
-
-	// create 10 accounts
-	var userIds [numberOfUsers]string
-	for i := 0; i < numberOfUsers; i++ {
-		userIds[i] = testCreateUserAccount(t, ctx, conn, i)
-	}
-
-	// make two bundles!
-	warehouse := pb.NewWarehouseClient(conn)
-	for n := 0; n < 2; n++ {
-		log.Println("Creating Bundle #", n)
-		stream, err := warehouse.StoreBundle(ctx)
-		assert.NoError(t, err)
-
-		for i := 0; i < numberOfUsers; i++ {
-			rawData := &pb.RawDataRequest{
-				CollectionId: collectionId,
-				OwnerId:      userIds[i],
-				Payload:      fmt.Sprintf("{\"name\":\"%s\",\"age\":%d}", userIds[i], i),
-			}
-			assert.NoError(t, stream.Send(rawData), "datum", rawData.String())
-		}
-
-		result, err := stream.CloseAndRecv()
-		assert.NoError(t, err)
-
-		log.Println("Stored URI:", result.Uri)
-		log.Println("Stored Data Count:", result.DataCount)
-		log.Println("Bundle ID:", result.BundleId)
-	}
+	main()
 }
