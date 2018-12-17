f590170576d67e7271ad9d6b56478b11f71395bb
therne <retail3210@gmail.com>
2018-12-17 14:58:48 +0900

    serverapi: Add Accounts.Exists API

---
+++ node/serverapi/accounts_api.go
@@ -0,0 +1,32 @@
+package serverapi
+
+import (
+	"context"
+	"github.com/airbloc/airbloc-go/account"
+	"github.com/airbloc/airbloc-go/node"
+	pb "github.com/airbloc/airbloc-go/proto/rpc/v1/server"
+)
+
+type AccountsAPI struct {
+	accounts *account.Manager
+}
+
+func NewAccountsAPI(backend node.Backend) (node.API, error) {
+	return &AccountsAPI{
+		accounts: account.NewManager(backend.Client()),
+	}, nil
+}
+
+func (api *AccountsAPI) Exists(ctx context.Context, req *pb.AccountExistsRequest) (*pb.AccountExistsResponse, error) {
+	_, err := api.accounts.GetByIdentity(req.GetIdentity())
+	if err == account.ErrNoAccount {
+		return &pb.AccountExistsResponse{Exists: false}, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return &pb.AccountExistsResponse{Exists: true}, nil
+}
+
+func (api *AccountsAPI) AttachToAPI(service *node.APIService) {
+	pb.RegisterAccountsServer(service.GrpcServer, api)
+}
