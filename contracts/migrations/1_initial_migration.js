20c84564a318e3cd429d066e0a89dccb79b8d316
therne <retail3210@gmail.com>
2019-01-03 21:31:28 +0900

    common: Friendly debug message

--- contracts/migrations/1_initial_migration.js
+++ contracts/migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function(deployer, network, accounts) {
+    console.log(accounts[0]);
+    deployer.deploy(Migrations);
 };
