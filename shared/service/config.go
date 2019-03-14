b5c4dfa75d20416034ae3d3fd4cb4ff949bf90f0
hongbyeongsu <frostornge@gmail.com>
2019-03-14 14:17:02 +0900

    warehouse : Move warehouse.Config to service.Config
    * 분리되어있던 warehouse의 config를 service로 merge하였습니다.

--- shared/service/config.go
+++ shared/service/config.go
@@ -1,8 +1,8 @@
 package service
 
 import (
-	"github.com/airbloc/airbloc-go/shared/warehouse"
 	"github.com/mcuadros/go-defaults"
+	"time"
 )
 
 type Config struct {
@@ -34,7 +34,33 @@ type Config struct {
 		DeploymentPath string `default:"deployment.local.json" yaml:"deploymentPath"`
 	} `yaml:"blockchain"`
 
-	Warehouse warehouse.Config
+	Warehouse struct {
+		DefaultStorage string `default:"local" yaml:"defaultStorage"`
+
+		Http struct {
+			Timeout         time.Duration `default:"30s"`
+			MaxConnsPerHost int           `default:"5" yaml:"maxConnsPerHost"`
+		}
+
+		LocalStorage struct {
+			SavePath string `default:"local/warehouse"`
+			Endpoint string `default:"http://localhost:80"`
+		}
+
+		S3 struct {
+			Region     string `default:"ap-northeast-1" yaml:"region"`
+			AccessKey  string `yaml:"accessKey"`
+			SecretKey  string `yaml:"secretKey"`
+			Token      string `default:"" yaml:"token"`
+			Bucket     string `yaml:"bucket"`
+			PathPrefix string `yaml:"prefix"`
+		}
+
+		Debug struct {
+			DisableUserAuthValidation bool `default:"false" yaml:"disableUserAuthValidation"`
+			DisableSchemaValidation   bool `default:"false" yaml:"disableSchemaValidation"`
+		}
+	} `yaml:"warehouse"`
 
 	UserDelegate struct {
 		AccountIds []string `yaml:"accountIds"`
