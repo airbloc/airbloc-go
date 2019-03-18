0211e62696f8ef91d4d181bd5f866180d22b38c5
therne <retail3210@gmail.com>
2019-03-18 11:55:11 +0900

    common: Add "nobuild" option to launch.sh

--- launch.sh
+++ launch.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-make airbloc &> ./build/airbloc.log
-if [[ $? -eq 0 ]]; then
-    rm ./build/airbloc.log
-    echo Build successful.
-    sleep 1
+if [[ $1 = "nobuild" ]]; then
+    echo Skipping Build
 else
-    cat ./build/airbloc.log
-    echo Build failed. See log for details.
-    exit 1
+    make airbloc &> ./build/airbloc.log
+    if [[ $? -eq 0 ]]; then
+        rm ./build/airbloc.log
+        echo Build successful.
+        sleep 1
+    else
+        cat ./build/airbloc.log
+        echo Build failed. See log for details.
+        exit 1
+    fi
 fi
-
 
 # Launch Airbloc tmux
 tmux new -d -s airbloc
