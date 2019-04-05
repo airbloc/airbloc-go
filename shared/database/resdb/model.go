2f5015ce7b9cf4f62ccf1cf27f3e411723c4564d
hongbyeongsu <frostornge@gmail.com>
2019-04-05 15:33:22 +0900

    common: Remove line break

--- shared/database/resdb/model.go
+++ shared/database/resdb/model.go
@@ -17,25 +17,14 @@ func NewModel(client afclient.Client, typ string) Model {
 	}
 }
 
-func (m Model) Get(
-	ctx context.Context,
-	id string,
-) (*afclient.Object, error) {
+func (m Model) Get(ctx context.Context, id string) (*afclient.Object, error) {
 	return m.client.Get(ctx, m.typ, id)
 }
 
-func (m Model) Put(
-	ctx context.Context,
-	id string,
-	data afclient.M,
-) (*afclient.PutResult, error) {
+func (m Model) Put(ctx context.Context, id string, data afclient.M) (*afclient.PutResult, error) {
 	return m.client.Put(ctx, m.typ, id, data)
 }
 
-func (m Model) Query(
-	ctx context.Context,
-	query afclient.M,
-	opts ...afclient.QueryOption,
-) ([]*afclient.Object, error) {
+func (m Model) Query(ctx context.Context, query afclient.M, opts ...afclient.QueryOption) ([]*afclient.Object, error) {
 	return m.client.Query(ctx, m.typ, query, opts...)
 }
