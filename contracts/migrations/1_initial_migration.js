1ff62599d3272b9980ff3b2eb4ab6e8a57c86f57
hongbyeongsu <frostornge@gmail.com>
2019-07-22 20:06:01 +0900

    contracts: Remove `contracts` & generate new version of adapter

--- contracts/migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-
-module.exports = function(deployer, network, accounts) {
-    console.log(accounts[0]);
-    deployer.deploy(Migrations);
-};
