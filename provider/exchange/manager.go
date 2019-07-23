da4bfa95c31b1f5ad2ee606bcc6313dbe1b6a51b
hongbyeongsu <frostornge@gmail.com>
2019-07-23 19:01:34 +0900

    provider/exchange: Remove long struct type declaration

--- provider/exchange/manager.go
+++ provider/exchange/manager.go
@@ -1,8 +1,6 @@
 package exchange
 
 import (
-	"math/big"
-
 	"github.com/airbloc/airbloc-go/shared/adapter"
 	"github.com/airbloc/airbloc-go/shared/blockchain"
 	"github.com/airbloc/airbloc-go/shared/types"
@@ -119,18 +117,6 @@ func (manager *Manager) Cancel(ctx context.Context, offerId types.ID) error {
 	return err
 }
 
-func (manager *Manager) GetOffer(offerId types.ID) (struct {
-	Provider string
-	Consumer common.Address
-	DataIds  [][20]byte
-	At       *big.Int
-	Until    *big.Int
-	Escrow   struct {
-		Addr common.Address
-		Sign [4]byte
-		Args []byte
-	}
-	Status uint8
-}, error) {
+func (manager *Manager) GetOffer(offerId types.ID) (types.Offer, error) {
 	return manager.contract.GetOffer(nil, offerId)
 }
