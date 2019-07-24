122a530e59be9e4616b4312162ce11a3b2783fa8
hongbyeongsu <frostornge@gmail.com>
2019-07-24 14:36:43 +0900

    provider/apps: Add simple comments

--- provider/api/apps.go
+++ provider/api/apps.go
@@ -13,33 +13,44 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AppRegistryAPI is api wrapper of contract AppRegistry.sol
 type AppRegistryAPI struct {
 	apps *apps.Manager
 }
 
+// NewAppRegistryAPI makes new *AppRegistryAPI struct
 func NewAppRegistryAPI(backend service.Backend) (api.API, error) {
 	ar := apps.NewManager(backend.Client())
 	return &AppRegistryAPI{ar}, nil
 }
 
+// Register is a paid mutator transaction binding the contract method 0xf2c298be.
+//
+// Solidity: function register(string appName) returns()
 func (api *AppRegistryAPI) Register(c *gin.Context) {
 	appName := c.Param(":appName")
-	if err := api.apps.Register(appName); err != nil {
+	if err := api.apps.Register(c, appName); err != nil {
 		c.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": err})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
+// Unregister is a paid mutator transaction binding the contract method 0x6598a1ae.
+//
+// Solidity: function unregister(string appName) returns()
 func (api *AppRegistryAPI) Unregister(c *gin.Context) {
 	appName := c.Param(":appName")
-	if err := api.apps.Unregister(appName); err != nil {
+	if err := api.apps.Unregister(c, appName); err != nil {
 		c.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": err})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
+// Get is a free data retrieval call binding the contract method 0x693ec85e.
+//
+// Solidity: function get(string appName) constant returns((string,address,bytes32))
 func (api *AppRegistryAPI) Get(c *gin.Context) {
 	appName := c.Param(":appName")
 
@@ -51,6 +62,9 @@ func (api *AppRegistryAPI) Get(c *gin.Context) {
 	c.JSON(http.StatusOK, app)
 }
 
+// TransferAppOwner is a paid mutator transaction binding the contract method 0x1a9dff9f.
+//
+// Solidity: function transferAppOwner(string appName, address newOwner) returns()
 func (api *AppRegistryAPI) TransferAppOwner(c *gin.Context) {
 	appName := c.Param(":appName")
 
@@ -60,13 +74,14 @@ func (api *AppRegistryAPI) TransferAppOwner(c *gin.Context) {
 	}
 
 	newOwner := common.HexToAddress(req.NewOwner)
-	if err := api.apps.TransferAppOwner(appName, newOwner); err != nil {
+	if err := api.apps.TransferAppOwner(c, appName, newOwner); err != nil {
 		c.AbortWithStatusJSON(http.StatusConflict, gin.H{"message": err})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
+// AttachToAPI is a registrant of an api.
 func (api *AppRegistryAPI) AttachToAPI(service *api.Service) {
 	apiMux := service.RestAPIMux.Group("/apps")
 	apiMux.GET("/:appName", api.Register)
--- provider/apps/manager.go
+++ provider/apps/manager.go
@@ -1,17 +1,21 @@
 package apps
 
 import (
+	"context"
+
 	"github.com/airbloc/airbloc-go/shared/adapter"
 	"github.com/airbloc/airbloc-go/shared/blockchain"
 	"github.com/airbloc/airbloc-go/shared/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// Manager is contract wrapper struct
 type Manager struct {
 	client   blockchain.TxClient
 	contract *adapter.AppRegistry
 }
 
+// NewManager makes new *Manager struct
 func NewManager(client blockchain.TxClient) *Manager {
 	contract := client.GetContract(&adapter.AppRegistry{})
 	return &Manager{
@@ -20,7 +24,10 @@ func NewManager(client blockchain.TxClient) *Manager {
 	}
 }
 
-func (manager *Manager) Register(appName string) error {
+// Register is a paid mutator transaction binding the contract method 0xf2c298be.
+//
+// Solidity: function register(string appName) returns()
+func (manager *Manager) Register(ctx context.Context, appName string) error {
 	tx, err := manager.contract.Register(manager.client.Account(), appName)
 	if err != nil {
 		return err
@@ -35,7 +42,10 @@ func (manager *Manager) Register(appName string) error {
 	return err
 }
 
-func (manager *Manager) Unregister(appName string) error {
+// Unregister is a paid mutator transaction binding the contract method 0x6598a1ae.
+//
+// Solidity: function unregister(string appName) returns()
+func (manager *Manager) Unregister(ctx context.Context, appName string) error {
 	tx, err := manager.contract.Unregister(manager.client.Account(), appName)
 	if err != nil {
 		return err
@@ -51,11 +61,17 @@ func (manager *Manager) Unregister(appName string) error {
 
 }
 
+// Get is a free data retrieval call binding the contract method 0x693ec85e.
+//
+// Solidity: function get(string appName) constant returns((string,address,bytes32))
 func (manager *Manager) Get(appName string) (types.App, error) {
 	return manager.contract.Get(nil, appName)
 }
 
-func (manager *Manager) TransferAppOwner(appName string, newOwner common.Address) error {
+// TransferAppOwner is a paid mutator transaction binding the contract method 0x1a9dff9f.
+//
+// Solidity: function transferAppOwner(string appName, address newOwner) returns()
+func (manager *Manager) TransferAppOwner(ctx context.Context, appName string, newOwner common.Address) error {
 	tx, err := manager.contract.TransferAppOwner(manager.client.Account(), appName, newOwner)
 	if err != nil {
 		return err
