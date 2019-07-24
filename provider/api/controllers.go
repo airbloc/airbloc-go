3d54b0aedd49e0afa6fb0e6cbea7b935bfafb1d5
hongbyeongsu <frostornge@gmail.com>
2019-07-24 14:49:24 +0900

    provider/controllers: Add simple comments

--- provider/api/controllers.go
+++ provider/api/controllers.go
@@ -10,15 +10,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ControllerRegistryAPI is api wrapper of contract ControllerRegistry.sol
 type ControllerRegistryAPI struct {
 	controllers *controllers.Manager
 }
 
+// NewControllerRegistryAPI makes new *ControllerRegistryAPI struct
 func NewControllerRegistryAPI(backend service.Backend) (api.API, error) {
 	cr := controllers.NewManager(backend.Client())
 	return &ControllerRegistryAPI{cr}, nil
 }
 
+// Register is a paid mutator transaction binding the contract method 0x4420e486.
+//
+// Solidity: function register(address controllerAddr) returns()
 func (api *ControllerRegistryAPI) Register(c *gin.Context) {
 	rawControllerAddr := c.Param("controllerAddr")
 	controllerAddr := common.HexToAddress(rawControllerAddr)
@@ -30,6 +35,9 @@ func (api *ControllerRegistryAPI) Register(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
+// Get is a free data retrieval call binding the contract method 0xc2bc2efc.
+//
+// Solidity: function get(address controller) constant returns((address,uint256))
 func (api *ControllerRegistryAPI) Get(c *gin.Context) {
 	rawControllerAddr := c.Param("controllerAddr")
 	controllerAddr := common.HexToAddress(rawControllerAddr)
@@ -42,6 +50,7 @@ func (api *ControllerRegistryAPI) Get(c *gin.Context) {
 	c.JSON(http.StatusOK, controller)
 }
 
+// AttachToAPI is a registrant of an api.
 func (api *ControllerRegistryAPI) AttachToAPI(service *api.Service) {
 	apiMux := service.RestAPIMux.Group("/controllers")
 	apiMux.GET("/:controllerAddr", api.Get)
--- provider/controllers/manager.go
+++ provider/controllers/manager.go
@@ -10,11 +10,13 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// Manager is contract wrapper struct
 type Manager struct {
 	client   blockchain.TxClient
 	contract *adapter.ControllerRegistry
 }
 
+// NewManager makes new *Manager struct
 func NewManager(client blockchain.TxClient) *Manager {
 	contract := client.GetContract(&adapter.ControllerRegistry{})
 	return &Manager{
@@ -23,6 +25,9 @@ func NewManager(client blockchain.TxClient) *Manager {
 	}
 }
 
+// Register is a paid mutator transaction binding the contract method 0x4420e486.
+//
+// Solidity: function register(address controllerAddr) returns()
 func (manager *Manager) Register(ctx context.Context, controllerAddr common.Address) error {
 	tx, err := manager.contract.Register(manager.client.Account(), controllerAddr)
 	if err != nil {
@@ -38,6 +43,9 @@ func (manager *Manager) Register(ctx context.Context, controllerAddr common.Addr
 	return err
 }
 
+// Get is a free data retrieval call binding the contract method 0xc2bc2efc.
+//
+// Solidity: function get(address controller) constant returns((address,uint256))
 func (manager *Manager) Get(ctx context.Context, controllerAddr common.Address) (types.DataController, error) {
 	return manager.contract.Get(nil, controllerAddr)
 }
