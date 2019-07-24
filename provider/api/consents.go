404acb42317bcdd6f7b32175b388e3773660986f
hongbyeongsu <frostornge@gmail.com>
2019-07-24 14:47:13 +0900

    provider/api: Change visibility of `ConsentsAPI`

--- provider/api/consents.go
+++ provider/api/consents.go
@@ -15,20 +15,20 @@ import (
 )
 
 // ConsentsAPI is api wrapper of contract Consents.sol
-type ConsentsAPI struct {
+type consentsAPI struct {
 	consents *consents.Manager
 }
 
 // NewConsentsAPI makes new *ConsentsAPI struct
 func NewConsentsAPI(backend service.Backend) (api.API, error) {
 	cs := consents.NewManager(backend.Client())
-	return &ConsentsAPI{cs}, nil
+	return &consentsAPI{cs}, nil
 }
 
 // Consent is a paid mutator transaction binding the contract method 0xbecae241.
 //
 // Solidity: function consent(uint8 action, string appName, string dataType, bool allowed) returns()
-func (api *ConsentsAPI) Consent(c *gin.Context) {
+func (api *consentsAPI) consent(c *gin.Context) {
 	var req struct {
 		Action   uint8
 		AppName  string
@@ -54,7 +54,7 @@ func (api *ConsentsAPI) Consent(c *gin.Context) {
 // ConsentByController is a paid mutator transaction binding the contract method 0xf92519d8.
 //
 // Solidity: function consentByController(uint8 action, bytes8 userId, string appName, string dataType, bool allowed) returns()
-func (api *ConsentsAPI) ConsentByController(c *gin.Context) {
+func (api *consentsAPI) consentByController(c *gin.Context) {
 	var req struct {
 		Action   uint8
 		UserId   string
@@ -87,7 +87,7 @@ func (api *ConsentsAPI) ConsentByController(c *gin.Context) {
 // ModifyConsentByController is a paid mutator transaction binding the contract method 0xedf2ef20.
 //
 // Solidity: function modifyConsentByController(uint8 action, bytes8 userId, string appName, string dataType, bool allowed, bytes passwordSignature) returns()
-func (api *ConsentsAPI) ModifyConsentByController(c *gin.Context) {
+func (api *consentsAPI) modifyConsentByController(c *gin.Context) {
 	var req struct {
 		Action            uint8
 		UserId            string
@@ -129,7 +129,7 @@ func (api *ConsentsAPI) ModifyConsentByController(c *gin.Context) {
 //
 // Solidity: function isAllowed(uint8 action, bytes8 userId, string appName, string dataType) constant returns(bool)
 // Solidity: function isAllowedAt(uint8 action, bytes8 userId, string appName, string dataType, uint256 blockNumber) constant returns(bool)
-func (api *ConsentsAPI) IsAllowed(c *gin.Context) {
+func (api *consentsAPI) isAllowed(c *gin.Context) {
 	var req struct {
 		Action      uint8
 		UserId      string
@@ -178,10 +178,10 @@ func (api *ConsentsAPI) IsAllowed(c *gin.Context) {
 }
 
 // AttachToAPI is a registrant of an api.
-func (api *ConsentsAPI) AttachToAPI(service *api.Service) {
+func (api *consentsAPI) AttachToAPI(service *api.Service) {
 	apiMux := service.RestAPIMux.Group("/consents")
-	apiMux.POST("/", api.Consent)
-	apiMux.GET("/allowed", api.IsAllowed)
-	apiMux.PUT("/controller", api.ModifyConsentByController)
-	apiMux.POST("/controller", api.ConsentByController)
+	apiMux.POST("/", api.consent)
+	apiMux.GET("/allowed", api.isAllowed)
+	apiMux.PUT("/controller", api.modifyConsentByController)
+	apiMux.POST("/controller", api.consentByController)
 }
