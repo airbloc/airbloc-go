554b2779de2e811829d033d0b52588923a779015
hongbyeongsu <frostornge@gmail.com>
2019-07-30 19:09:20 +0900

    provider/api: Add test file

--- provider/api/accounts_test.go
+++ provider/api/accounts_test.go
@@ -1,8 +1,67 @@
 package api
 
-import (
-	"testing"
-)
+//func TestNewAccountsAPI_Create(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
 
-func TestNewAccountsAPI(t *testing.T) {
-}
+//func TestNewAccountsAPI_CreateTemporary(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
+//
+//func TestNewAccountsAPI_UnlockTemporary(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
+//
+//func TestNewAccountsAPI_SetController(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
+//
+//func TestNewAccountsAPI_GetAccount(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
+//
+//func TestNewAccountsAPI_GetAccountId(t *testing.T) {
+//	mockController := gomock.NewController(t)
+//	defer mockController.Finish()
+//
+//	mockManager := mocks.NewMockIAccountsManager(mockController)
+//
+//	resp := httptest.NewRecorder()
+//	ctx, _ := gin.CreateTestContext(resp)
+//
+//}
