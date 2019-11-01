65dc5597872d7f565d9192e94a0ee10f35019ce4
hongbyeongsu <frostornge@gmail.com>
2019-11-01 16:50:55 +0900

    blockchain: Merge selector utils to contract manager struct

--- blockchain/manager.go
+++ blockchain/manager.go
@@ -1,34 +1,51 @@
 package blockchain
 
 import (
+	"fmt"
 	"reflect"
 
 	"github.com/airbloc/airbloc-go/bind"
 	"github.com/airbloc/airbloc-go/bind/managers"
 	"github.com/airbloc/airbloc-go/bind/wrappers"
+	"github.com/airbloc/logger"
 
+	"github.com/klaytn/klaytn/blockchain/types"
+	"github.com/klaytn/klaytn/common"
+	"github.com/klaytn/klaytn/common/hexutil"
 	"github.com/pkg/errors"
 )
 
-type ManagerConstructor func(bind.ContractBackend, interface{}) interface{}
-type ContractConstructor func(bind.Deployment, bind.ContractBackend) interface{}
+type constructorData struct {
+	manager  func(bind.ContractBackend, interface{}) interface{}
+	contract func(bind.Deployment, bind.ContractBackend) interface{}
+}
 
 var (
-	contractConstructors = map[string]ContractConstructor{
-		"Accounts":           wrappers.NewAccountsContract,
-		"AppRegistry":        wrappers.NewAppRegistryContract,
-		"Consents":           wrappers.NewConsentsContract,
-		"ControllerRegistry": wrappers.NewControllerRegistryContract,
-		"DataTypeRegistry":   wrappers.NewDataTypeRegistryContract,
-		"Exchange":           wrappers.NewExchangeContract,
-	}
-	managerConstructors = map[string]ManagerConstructor{
-		"Accounts":           managers.NewAccountsManager,
-		"AppRegistry":        managers.NewAppRegistryManager,
-		"Consents":           managers.NewConsentsManager,
-		"ControllerRegistry": managers.NewControllerRegistryManager,
-		"DataTypeRegistry":   managers.NewDataTypeRegistryManager,
-		"Exchange":           managers.NewExchangeManager,
+	constructors = map[string]constructorData{
+		"Accounts": {
+			contract: wrappers.NewAccountsContract,
+			manager:  managers.NewAccountsManager,
+		},
+		"AppRegistry": {
+			contract: wrappers.NewAppRegistryContract,
+			manager:  managers.NewAppRegistryManager,
+		},
+		"Consents": {
+			contract: wrappers.NewConsentsContract,
+			manager:  managers.NewConsentsManager,
+		},
+		"ControllerRegistry": {
+			contract: wrappers.NewControllerRegistryContract,
+			manager:  managers.NewControllerRegistryManager,
+		},
+		"DataTypeRegistry": {
+			contract: wrappers.NewDataTypeRegistryContract,
+			manager:  managers.NewDataTypeRegistryManager,
+		},
+		"Exchange": {
+			contract: wrappers.NewExchangeContract,
+			manager:  managers.NewExchangeManager,
+		},
 	}
 )
 
@@ -39,10 +56,16 @@ type ContractManager struct {
 	ControllerRegistry managers.IControllerRegistryManager
 	DataTypeRegistry   managers.IDataTypeRegistryManager
 	Exchange           managers.IExchangeManager
+
+	addrToName      map[common.Address]string
+	addrToSelectors map[common.Address]map[[4]byte]string
 }
 
 func NewContractManager(client bind.ContractBackend, deployments bind.Deployments) (ContractManager, error) {
-	m := ContractManager{}
+	m := ContractManager{
+		addrToName:      make(map[common.Address]string),
+		addrToSelectors: make(map[common.Address]map[[4]byte]string),
+	}
 	t := reflect.TypeOf(m)
 	v := reflect.ValueOf(&m)
 
@@ -51,13 +74,8 @@ func NewContractManager(client bind.ContractBackend, deployments bind.Deployment
 
 		name := field.Name
 
-		contractConstructor, ok := contractConstructors[name]
-		if !ok {
-			return ContractManager{}, errors.Errorf("constructor for %+v does not exist", name)
-		}
-
-		managerConstructor, ok := managerConstructors[name]
-		if !ok {
+		constructor, exist := constructors[name]
+		if !exist {
 			return ContractManager{}, errors.Errorf("constructor for %+v does not exist", name)
 		}
 
@@ -68,11 +86,61 @@ func NewContractManager(client bind.ContractBackend, deployments bind.Deployment
 			}
 		}
 
-		instance := managerConstructor(client, contractConstructor(deployment, client))
-		if reflect.TypeOf(instance).Implements(field.Type) {
-			v.Elem().FieldByName(name).Set(reflect.ValueOf(instance))
+		contract := constructor.contract(deployment, client)
+		if contractBase, ok := contract.(bind.ContractBase); ok {
+			manager := constructor.manager(client, contract)
+			if reflect.TypeOf(manager).Implements(field.Type) {
+				v.Elem().FieldByName(name).Set(reflect.ValueOf(manager))
+				m.addrToName[contractBase.Address()] = name
+				m.addrToSelectors[contractBase.Address()] = contractBase.GetSignatures()
+			}
 		}
 	}
 
 	return m, nil
+}
+
+func (cm ContractManager) getSignatureFromTxData(addr common.Address, txdata []byte) (string, error) {
+	if txdata == nil {
+		return "", errors.New("nil txdata")
+	}
+	if len(txdata) < 4 {
+		return "", errors.New("txdata too short")
+	}
+
+	var sign [4]byte
+	copy(sign[:], txdata[:4])
+
+	if signHex, registered := cm.addrToSelectors[addr][sign]; registered {
+		return signHex, nil
+	} else {
+		return hexutil.Encode(sign[:]), nil
+	}
+}
+
+func (cm ContractManager) GetTransactionDetails(tx *types.Transaction) (string, logger.Attrs, error) {
+	addr := tx.To()
+	if addr == nil {
+		return "", nil, errors.Wrap(errors.New("nil address"), "getting destination address from tx")
+	}
+
+	var methodName string
+	if name, registered := cm.addrToName[*addr]; registered {
+		signature, err := cm.getSignatureFromTxData(*addr, tx.Data())
+		if err != nil {
+			return "", nil, errors.Wrap(err, "getting signature from txdata")
+		}
+		methodName = fmt.Sprintf("%s.%s", name, signature)
+	} else {
+		methodName = addr.Hex()
+	}
+
+	attrs := logger.Attrs{
+		"txid": tx.Hash().Hex(),
+		"gas":  tx.Gas(),
+	}
+	if tx.Value() != nil {
+		attrs["value"] = fmt.Sprintf("%s klay", WeiToKlay(tx.Value()).Text('e', 2))
+	}
+	return methodName, attrs, nil
 }
--- blockchain/utils.go
+++ blockchain/utils.go
@@ -1,68 +1,15 @@
 package blockchain
 
 import (
-	"fmt"
 	"math/big"
 
-	"github.com/airbloc/logger"
-	"github.com/klaytn/klaytn/blockchain/types"
-	"github.com/klaytn/klaytn/common/hexutil"
 	"github.com/klaytn/klaytn/params"
 )
 
-var (
-	// SelectorToMethodSignature is filled by automatically generated contract binds in package `adapter`.
-	SelectorToMethodSignature = make(map[Selector]string)
-)
-
-// Selector is first 4-byte slice of hashed method signatures like "transfer(uint256,address)".
-type Selector [4]byte
-
-// RegisterSelector fills SelectorToMethodSignature with given 8-chars long heximedical selector and signature,
-// and called by automatically generated contract binds in package `adapter`.
-func RegisterSelector(selectorHex string, signature string) {
-	var selector Selector
-	byteSelector := hexutil.MustDecode(selectorHex)
-	copy(selector[:], byteSelector)
-	SelectorToMethodSignature[selector] = signature
-}
-
-func getSignatureFromTxData(txdata []byte) string {
-	var selector Selector
-	copy(selector[:], txdata[:4])
-
-	if signature, ok := SelectorToMethodSignature[selector]; ok {
-		return signature
-	}
-	// unregistered signature. shouldn't be possible!
-	return hexutil.Encode(txdata)
-}
-
-// GetTransactionDetails parses Ethereum transaction and
-// returns loggable, human-readable informations.
-func GetTransactionDetails(cm *contractManager, tx *types.Transaction) (methodInfo string, attrs logger.Attrs) {
-	if contractName, registered := cm.addrToName[*tx.To()]; registered {
-		// get
-		signature := getSignatureFromTxData(tx.Data())
-		methodInfo = fmt.Sprintf("%s.%s", contractName, signature)
-	} else {
-		// external contract which has no ABI information.
-		methodInfo = tx.To().Hex()
-	}
-
-	attrs = make(logger.Attrs)
-	attrs["txid"] = tx.Hash().Hex()
-	attrs["gas"] = tx.Gas()
-	if tx.Value().Int64() > 0 {
-		attrs["value"] = fmt.Sprintf("%s eth", WeiToEth(tx.Value()).Text('e', 2))
-	}
-	return
-}
-
-// WeiToEth converts Solidity uint256 value to Ether (1e18).
-func WeiToEth(wei *big.Int) *big.Float {
+// WeiToKlay converts Solidity uint256 value to Klay (1e18).
+func WeiToKlay(peb *big.Int) *big.Float {
 	return new(big.Float).Quo(
-		new(big.Float).SetInt(wei),
+		new(big.Float).SetInt(peb),
 		new(big.Float).SetInt64(params.KLAY),
 	)
 }
@@ -78,6 +25,10 @@ func getChainName(cid *big.Int) string {
 		return "Ethereum Rinkeby Test"
 	case "1000":
 		return "Klaytn Aspen Test"
+	case "1001":
+		return "Klaytn Baobab Test"
+	case "8217":
+		return "Klaytn Main"
 	}
-	return "Ethereum Private Test"
+	return "EVM Private"
 }
