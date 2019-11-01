9ea9f4b97c50493b0844ad6fb7d2704500cea855
hongbyeongsu <frostornge@gmail.com>
2019-11-01 14:17:43 +0900

    airbloc: Remove service package

--- shared/service/api/api.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "github.com/airbloc/airbloc-go/shared/service"
-
-type API interface {
-	AttachToAPI(api *Service)
-}
-
-type Constructor func(backend service.Backend) (API, error)
