ecad4d8758263cccbd239a26689733e3ab68890a
hongbyeongsu <frostornge@gmail.com>
2019-12-20 19:29:42 +0900

    p2p: Rearrange Node, Peer wrapper * metadata based field * internal used context

--- network/p2p/node.go
+++ network/p2p/node.go
@@ -3,6 +3,11 @@ package p2p
 import (
 	"context"
 	"encoding/hex"
+	"sync"
+
+	"github.com/airbloc/airbloc-go/network/p2p/message"
+
+	"github.com/klaytn/klaytn/common"
 
 	"github.com/airbloc/airbloc-go/network/p2p/handshake/identity"
 
@@ -19,15 +24,17 @@ import (
 	"github.com/pkg/errors"
 )
 
-const ecdhHandshakeMessage = ".airbloc_node_handshake"
+const (
+	KeyNodeAccount   = "abl.acc"
+	KeyNodeContext   = "abl.ctx"
+	KeyNodeHandlers  = "abl.handlers"
+	KeyNodeLogger    = "abl.logger"
+	KeyNodePeerStore = "abl.peerstore"
 
-type Node struct {
-	context contextGroup
+	ecdhHandshakeMessage = ".airbloc_node_handshake"
+)
 
-	node *noise.Node
-	acc  account.Account
-	log  *logger.Logger
-}
+type Node struct{ *noise.Node }
 
 func newNode(host string, port uint16, tp transport.Layer) (*noise.Node, error) {
 	param := noise.DefaultParams()
@@ -50,11 +57,16 @@ func newNode(host string, port uint16, tp transport.Layer) (*noise.Node, error) 
 	return node, nil
 }
 
-func NewNode(host string, port uint16, tp transport.Layer, account account.Account) (Node, error) {
-	node, err := newNode(host, port, tp)
+func NewNode(
+	parentContext context.Context,
+	host string, port uint16, tp transport.Layer,
+	account account.Account,
+) (Node, error) {
+	n, err := newNode(host, port, tp)
 	if err != nil {
 		return Node{}, err
 	}
+	node := Node{n}
 
 	ecdhBlock := ecdh.New()
 	ecdhBlock.WithHandshakeMessage(ecdhHandshakeMessage)
@@ -64,40 +76,89 @@ func NewNode(host string, port uint16, tp transport.Layer, account account.Accou
 	p.Register(aead.New())
 	p.Register(skademlia.New())
 	p.Register(identity.New(account))
-	p.Enforce(node)
+	p.Enforce(node.Node)
 
-	airblocNode := Node{
-		node: node,
-		acc:  account,
-		log:  logger.New("airbloc-p2p-node"),
-	}
-	registerNodeEventHandler(airblocNode)
+	node.Set(KeyNodeAccount, account)
+	node.Set(KeyNodeContext, newContext(context.WithCancel(parentContext)))
+	node.Set(KeyNodeHandlers, new(sync.Map))
+	// TODO - remove postfix address
+	node.Set(KeyNodeLogger, logger.New("abl-p2p"+"/"+account.Address().Hex()))
+	node.Set(KeyNodePeerStore, new(sync.Map))
 
-	return airblocNode, nil
+	registerNodeEventHandler(node)
+
+	return node, nil
 }
 
-func (n *Node) Start(parentContext context.Context) {
-	// nil check context
-	if parentContext == nil {
-		parentContext = context.Background()
-	}
-	n.context = newContextGroup(context.WithCancel(parentContext))
+func (n Node) account() account.Account {
+	return n.Get(KeyNodeAccount).(account.Account)
+}
 
+func (n Node) context() Context {
+	return n.Get(KeyNodeContext).(Context)
+}
+
+func (n Node) handlers() *sync.Map {
+	return n.Get(KeyNodeHandlers).(*sync.Map)
+}
+
+func (n Node) logger() *logger.Logger {
+	return n.Get(KeyNodeLogger).(*logger.Logger)
+}
+
+func (n Node) peerstore() *sync.Map {
+	return n.Get(KeyNodePeerStore).(*sync.Map)
+}
+
+func (n Node) RegisterHandler(opcode noise.Opcode, handler message.HandlerFunc) {
+	n.handlers().Store(opcode, handler)
+}
+
+func (n Node) GetHandler(opcode noise.Opcode) (message.HandlerFunc, error) {
+	handler, exist := n.handlers().Load(opcode)
+	if !exist {
+		return nil, errors.New("handler that matches given opcode does not registered")
+	}
+	return handler.(message.HandlerFunc), nil
+}
+
+func (n Node) RegisterPeer(address common.Address, peer *noise.Peer) (exist bool) {
+	_, exist = n.peerstore().LoadOrStore(address, peer)
+	if !exist {
+		n.logger().Debug("Peer registered, address is {}", address.Hex())
+	} else {
+		n.logger().Debug("Peer duplicated, disconnecting {}", address.Hex())
+	}
+	return
+}
+
+func (n Node) UnregisterPeer(address common.Address) {
+	if _, exist := n.peerstore().Load(address); exist {
+		n.peerstore().Delete(address)
+		n.logger().Debug("Peer unregistered, address is {}", address.Hex())
+	}
+}
+
+func (n Node) Start() {
 	// listen
-	go n.node.Listen()
+	go n.Listen()
 
 	// start killer
 	go func() {
-		<-n.context.Done()
-		n.node.Kill()
-		n.log.Info("Node service closing...")
+		<-n.context().Done()
+		n.peerstore().Range(func(key, value interface{}) bool {
+			value.(*noise.Peer).Disconnect()
+			return true
+		})
+		n.Kill()
+		n.logger().Info("Node service closing...")
 	}()
-	n.log.Info("Node listening at {}", n.node.ExternalAddress())
+	n.logger().Info("Node listening at {}", n.ExternalAddress())
 }
 
-func (n *Node) Bootstrap(nodeAddresses ...string) error {
+func (n Node) Bootstrap(nodeAddresses ...string) error {
 	for _, nodeAddress := range nodeAddresses {
-		peer, err := n.node.Dial(nodeAddress)
+		peer, err := n.Dial(nodeAddress)
 		if err != nil {
 			n.Stop()
 			return errors.Wrapf(err, "dialing initial node, address: %s", nodeAddress)
@@ -105,20 +166,20 @@ func (n *Node) Bootstrap(nodeAddresses ...string) error {
 		skademlia.WaitUntilAuthenticated(peer)
 	}
 
-	peers := skademlia.FindNode(n.node, protocol.NodeID(n.node).(skademlia.ID), skademlia.BucketSize(), 8)
+	peers := skademlia.FindNode(n.Node, protocol.NodeID(n.Node).(skademlia.ID), skademlia.BucketSize(), 8)
 	peerAddresses := make([]string, len(peers))
 	for index, peer := range peers {
 		peerAddresses[index] = hex.EncodeToString(peer.PublicKey())
 	}
-	n.log.Info("Bootstrapped with {} peers", len(peers), logger.Attrs{"peers": peerAddresses})
+	n.logger().Info("Bootstrapped with {} peers", len(peers), logger.Attrs{"peers": peerAddresses})
 	return nil
 }
 
-func (n *Node) StartWithInitialNodes(parentContext context.Context, nodeAddresses ...string) error {
-	n.Start(parentContext)
+func (n Node) StartWithInitialNodes(nodeAddresses ...string) error {
+	n.Start()
 	return n.Bootstrap(nodeAddresses...)
 }
 
 func (n Node) Stop() {
-	n.context.Cancel()
+	n.context().Cancel()
 }
--- network/p2p/peer.go
+++ network/p2p/peer.go
@@ -1,24 +1,153 @@
 package p2p
 
 import (
+	"reflect"
+	"sync"
 	"time"
+
+	"github.com/airbloc/logger"
+
+	"github.com/airbloc/airbloc-go/network/p2p/message"
 
 	"github.com/airbloc/airbloc-go/network/p2p/handshake/identity"
 	"github.com/klaytn/klaytn/common"
 	"github.com/perlin-network/noise"
+	"github.com/pkg/errors"
 )
 
-func GetPeerAddressAsync(peer *noise.Peer) <-chan common.Address {
+const (
+	KeyPeerContext                    = "abl.ctx"
+	KeyPeerAggregatedMessageChannel   = "abl.ch.msg.aggr"
+	KeyPeerWaitGroupMessageWorker     = "abl.wg.msg.wkr"
+	KeyPeerWaitGroupMessageAggregator = "abl.wg.msg.aggr"
+)
+
+type Peer struct {
+	*noise.Peer
+}
+
+type aggregatedMessage struct {
+	message noise.Message
+	opcode  noise.Opcode
+}
+
+func (peer Peer) context() Context {
+	return peer.Get(KeyPeerContext).(Context)
+}
+
+func (peer Peer) GetAddressAsync() <-chan common.Address {
 	peerAddressChan := make(chan common.Address, 1)
 	go func() {
-		for peer.Get(identity.KeyAddress) == nil {
-			time.Sleep(100 * time.Millisecond)
+		timeout := time.After(2 * time.Second)
+		for {
+			select {
+			case <-timeout:
+				peerAddressChan <- common.Address{}
+				return
+			default:
+				peerAddress := peer.Get(identity.KeyAddress)
+				if peerAddress != nil {
+					peerAddressChan <- peerAddress.(common.Address)
+					return
+				}
+				time.Sleep(100 * time.Millisecond)
+			}
 		}
-		peerAddressChan <- peer.Get(identity.KeyAddress).(common.Address)
 	}()
 	return peerAddressChan
 }
 
-func GetPeerAddress(peer *noise.Peer) common.Address {
-	return <-GetPeerAddressAsync(peer)
+func (peer Peer) GetAddress() (common.Address, error) {
+	peerAddress := <-peer.GetAddressAsync()
+	if peerAddress == (common.Address{}) {
+		return common.Address{}, errors.New("cannot get peer's address")
+	}
+	return peerAddress, nil
+}
+
+func (peer Peer) aggregatedMessageChannnel() chan aggregatedMessage {
+	return peer.Get(KeyPeerAggregatedMessageChannel).(chan aggregatedMessage)
+}
+
+func (peer Peer) messageWorkerWaitGroup() *sync.WaitGroup {
+	return peer.Get(KeyPeerWaitGroupMessageWorker).(*sync.WaitGroup)
+}
+
+func (peer Peer) messageAggregatorWaitGroup() *sync.WaitGroup {
+	return peer.Get(KeyPeerWaitGroupMessageAggregator).(*sync.WaitGroup)
+}
+
+func RunMessageAggregator(node Node, peer Peer) {
+	var (
+		aggregatorWaitGroup                            = peer.messageAggregatorWaitGroup()
+		aggregatedMessageChan chan<- aggregatedMessage = peer.aggregatedMessageChannnel()
+	)
+
+	aggregatorWaitGroup.Add(len(message.Opcodes))
+	for _, opcode := range message.Opcodes {
+		go func(opcode noise.Opcode) {
+			defer aggregatorWaitGroup.Done()
+			for {
+				select {
+				case <-peer.context().Done():
+					return
+				case receivedMessage := <-peer.Receive(opcode):
+					aggregatedMessageChan <- struct {
+						message noise.Message
+						opcode  noise.Opcode
+					}{
+						message: receivedMessage,
+						opcode:  opcode,
+					}
+				}
+			}
+		}(opcode)
+	}
+}
+
+func RunMessageWorker(node Node, peer Peer) {
+	var (
+		workerWaitgroup                                = peer.messageWorkerWaitGroup()
+		aggregatedMessageChan <-chan aggregatedMessage = peer.aggregatedMessageChannnel()
+	)
+
+	workerWaitgroup.Add(1)
+	go func() {
+		defer workerWaitgroup.Done()
+
+		for {
+			select {
+			case <-peer.context().Done():
+				return
+			case receivedMessage := <-aggregatedMessageChan:
+				node.logger().Debug("Message received", logger.Attrs{
+					"opcode": uint8(receivedMessage.opcode),
+					"type":   reflect.TypeOf(receivedMessage.message).String(),
+				})
+				handler, err := node.GetHandler(receivedMessage.opcode)
+				if err != nil {
+					node.logger().Error("Cannot get handler", logger.Attrs{
+						"error": logger.Attrs{
+							"type":    reflect.TypeOf(errors.Cause(err)).String(),
+							"message": err.Error(),
+						},
+						"message": reflect.TypeOf(receivedMessage.message).String(),
+					})
+					continue
+				}
+
+				err = handler(node.context(), receivedMessage.message, peer.Peer)
+				if err != nil {
+					node.logger().Error("Handler error occurred", logger.Attrs{
+						"error": logger.Attrs{
+							"type":    reflect.TypeOf(errors.Cause(err)).String(),
+							"message": err.Error(),
+						},
+						"message": reflect.TypeOf(receivedMessage.message).String(),
+					})
+					continue
+				}
+			}
+		}
+	}()
 }
